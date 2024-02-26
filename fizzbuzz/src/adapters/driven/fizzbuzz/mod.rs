6f33bf356a2e749ef9c06a1408f4161aa7e5d33f
jerome bourgeois <jerome.bourgeois@adevinta.com>
2024-02-26 15:32:36 +0100

    fizzbuzz return a vector strings instead of a string

--- fizzbuzz/src/adapters/driven/fizzbuzz/mod.rs
+++ fizzbuzz/src/adapters/driven/fizzbuzz/mod.rs
@@ -1,19 +1,19 @@
 pub mod fizzbuzz {
 
-    pub fn fizzbuzz (int1: i32, int2: i32, limit: i32, str1: String, str2: String ) -> String {
-        let mut result = String::new();
+    pub fn fizzbuzz (int1: i32, int2: i32, limit: i32, str1: String, str2: String ) -> Vec<String> {
+        let mut result = Vec::new();
         let mut n = 1;
 
         while n <= limit {
             if n % int1 == 0 && n % int2 == 0 {
-                result = format!("{result}{str1}{str2}-");
+                result.push(format!("{str1}{str2}"));
             } else if n % int1 == 0 {
-                result = format!("{result}{str1}-");
+                result.push(format!("{str1}"));
             } else if n % int2 == 0 {
-                result = format!("{result}{str2}-");
+                result.push(format!("{str2}"));
             } else {
-                let str_int2 = int2.to_string();
-                result = format!("{result}{str_int2}-");
+                let str_n = n.to_string();
+                result.push(format!("{str_n}"));
             }
             n = n + 1;
         }
@@ -28,7 +28,8 @@ pub mod fizzbuzz {
         #[test]
         fn success() {
             let truc = fizzbuzz(2, 4, 10, String::from("fizz"), String::from("buzz"));
-            assert_eq!(truc, String::new())
+            let expected = vec!["1", "fizz", "3", "fizzbuzz", "5", "fizz", "7", "fizzbuzz", "9", "fizz"];
+            assert_eq!(truc, expected)
         }
     }
 }
