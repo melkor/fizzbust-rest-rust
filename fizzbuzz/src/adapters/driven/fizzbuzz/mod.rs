04009e0897ab810e051ffb2bb33682840b97dea7
jerome bourgeois <jerome.bourgeois@adevinta.com>
2024-02-26 16:17:43 +0100

    fizzbuzz handle error

--- fizzbuzz/src/adapters/driven/fizzbuzz/mod.rs
+++ fizzbuzz/src/adapters/driven/fizzbuzz/mod.rs
@@ -1,8 +1,20 @@
 pub mod fizzbuzz {
 
-    pub fn fizzbuzz (int1: i32, int2: i32, limit: i32, str1: String, str2: String ) -> Vec<String> {
+    pub fn fizzbuzz (int1: i32, int2: i32, limit: i32, str1: String, str2: String ) -> Result<Vec<String>, String> {
         let mut result = Vec::new();
         let mut n = 1;
+
+        if int1 < 1 {
+            return Err("int1 is invalid".to_string())
+        }
+
+        if int2 < 1 {
+            return Err("int2 is invalid".to_string())
+        }
+
+        if limit < 1 || limit > 100_000 {
+            return Err("limit is invalid".to_string())
+        }
 
         while n <= limit {
             if n % int1 == 0 && n % int2 == 0 {
@@ -18,7 +30,7 @@ pub mod fizzbuzz {
             n = n + 1;
         }
 
-        result
+        Ok(result)
     }
 
     #[cfg(test)]
@@ -27,9 +39,47 @@ pub mod fizzbuzz {
 
         #[test]
         fn success() {
-            let truc = fizzbuzz(2, 4, 10, String::from("fizz"), String::from("buzz"));
             let expected = vec!["1", "fizz", "3", "fizzbuzz", "5", "fizz", "7", "fizzbuzz", "9", "fizz"];
-            assert_eq!(truc, expected)
+            match fizzbuzz(2, 4, 10, String::from("fizz"), String::from("buzz")) {
+                Ok(actual) => assert_eq!(actual, expected),
+                Err(err) => assert_eq!(err, "")
+            }
+        }
+
+        #[test]
+        fn error_int1() {
+            let expected: Vec<String> = Vec::new();
+            match fizzbuzz(0, 4, 10, String::from("fizz"), String::from("buzz")) {
+                Ok(actual) => assert_eq!(actual, expected),
+                Err(err) => assert_eq!(err, "int1 is invalid")
+            }
+        }
+
+        #[test]
+        fn error_int2() {
+            let expected: Vec<String> = Vec::new();
+            match fizzbuzz(2, 0, 10, String::from("fizz"), String::from("buzz")) {
+                Ok(actual) => assert_eq!(actual, expected),
+                Err(err) => assert_eq!(err, "int2 is invalid")
+            }
+        }
+
+        #[test]
+        fn error_limit_to_small() {
+            let expected: Vec<String> = Vec::new();
+            match fizzbuzz(2, 4, 0, String::from("fizz"), String::from("buzz")) {
+                Ok(actual) => assert_eq!(actual, expected),
+                Err(err) => assert_eq!(err, "limit is invalid")
+            }
+        }
+
+        #[test]
+        fn error_limit_to_big() {
+            let expected: Vec<String> = Vec::new();
+            match fizzbuzz(2, 4, 200_000, String::from("fizz"), String::from("buzz")) {
+                Ok(actual) => assert_eq!(actual, expected),
+                Err(err) => assert_eq!(err, "limit is invalid")
+            }
         }
     }
 }
