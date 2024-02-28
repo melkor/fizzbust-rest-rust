76330da8c40892d25b84880c88326efdab1fa8fd
jerome bourgeois <jerome.bourgeois@adevinta.com>
2024-02-28 09:57:03 +0100

    add stub fizzbuzz route

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get,App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
+use serde::{Deserialize, Serialize};
 
 mod adapters;
 mod domains;
@@ -9,11 +10,42 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[derive(Deserialize)]
+struct FizzBuzzQuery {
+    int1: i32,
+    int2: i32,
+    limit: i32,
+    str1: String,
+    str2: String,
+}
+
+#[derive(Serialize)]
+struct FizzBuzzReponse {
+    int1: i32,
+    int2: i32,
+    limit: i32,
+    str1: String,
+    str2: String,
+}
+
+#[get("/fizzbuzz")]
+async fn get_fizzbuzz(query: web::Query<FizzBuzzQuery>) -> Result<impl Responder> {
+    let response = FizzBuzzReponse {
+        int1: query.int1,
+        int2: query.int2,
+        limit: query.limit,
+        str1: query.str1.to_string(),
+        str2: query.str2.to_string(),
+    };
+    Ok(web::Json(response))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .service(get_fizzbuzz)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
