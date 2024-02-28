bd8410b4d83d79d360977006a7960580e588f1f7
jerome bourgeois <jerome.bourgeois@adevinta.com>
2024-02-28 09:23:13 +0100

    add actix dependenci

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,7 +1,21 @@
+use actix_web::{get,App, HttpResponse, HttpServer, Responder};
+
 mod adapters;
 mod domains;
 mod ports;
 
-fn main () {
-    
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
